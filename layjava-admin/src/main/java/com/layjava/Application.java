1e139074f389a130d55d1b853f46d2ca2ea4dba0
chengliang4810 <chengliang4810@163.com>
2024-02-25 23:53:49 +0800

    初始化项目结构，填写相关依赖

---
+++ layjava-admin/src/main/java/com/layjava/Application.java
@@ -0,0 +1,13 @@
+package com.layjava;
+
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.SolonMain;
+
+@SolonMain
+public class Application {
+
+    public static void main(String[] args) {
+        Solon.start(Application.class, args);
+    }
+
+}
