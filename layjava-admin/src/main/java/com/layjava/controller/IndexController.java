03c47fafd7b28518b6e6d8109d3d8225447786a2
chengliang4810 <chengliang4810@163.com>
2024-03-27 22:11:16 +0800

    feat: 添加默认Controller

---
+++ layjava-admin/src/main/java/com/layjava/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.layjava.controller;
+
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+
+/**
+ * 后端默认 Controller，可用于监测服务是否启动成功
+ * @author chengliang
+ * @date 2024/03/27
+ */
+@Controller
+public class IndexController {
+
+    @Mapping("/")
+    public String index(){
+        return "欢迎使用LayJava-Admin";
+    }
+
+}
