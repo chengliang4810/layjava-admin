39924ab2456358a28948004c0b80981286609496
chengliang4810 <chengliang4810@163.com>
2024-03-29 01:10:15 +0800

    test: 查询数据测试代码

--- layjava-modules/layjava-system/src/main/java/com/layjava/system/test/controller/TestController.java
+++ layjava-modules/layjava-system/src/main/java/com/layjava/system/test/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.layjava.system.test.controller;
 
+import cn.dev33.satoken.annotation.SaIgnore;
+import com.layjava.system.test.domain.Test;
+import com.layjava.system.test.mapper.TestMapper;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Get;
+import org.noear.solon.annotation.Inject;
 import org.noear.solon.annotation.Mapping;
+
+import java.util.List;
 
 /**
  * 测试控制器
@@ -11,12 +17,18 @@ import org.noear.solon.annotation.Mapping;
  * @date 2024/02/26
  */
 @Controller
-@Mapping("/test")
+@SaIgnore
+@Mapping("/hello")
 public class TestController {
+
+    @Inject
+    private TestMapper testMapper;
 
     @Get
     @Mapping("/demo")
     public String test() {
+        List<Test> tests = testMapper.selectList(null);
+        System.out.println(tests);
         return "test";
     }
 
---
+++ layjava-modules/layjava-system/src/main/java/com/layjava/system/test/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.layjava.system.test.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.layjava.system.test.domain.Test;
+
+public interface TestMapper extends BaseMapper<Test> {
+
+
+}
