9c5b7bd9551f33af9a3fd1363c2e92f72f3196f0
chengliang <chengliang4810@163.com>
2024-04-24 14:51:50 +0800

    feat: 新增用户通用接口

---
+++ layjava-modules/layjava-system/src/main/java/com/layjava/system/mapper/SysUserMapper.java
@@ -0,0 +1,16 @@
+package com.layjava.system.mapper;
+
+import com.layjava.system.domain.SysUser;
+import com.layjava.system.domain.vo.SysUserVo;
+import com.layjava.common.mybatis.core.mapper.BaseMapperPlus;
+
+/**
+ *
+ * 用户信息表 Mapper 接口
+ *
+ * @author chengliang4810
+ * @since 2024-04-24
+ */
+public interface SysUserMapper extends BaseMapperPlus<SysUser, SysUserVo> {
+
+}
---
+++ layjava-modules/layjava-system/src/main/java/com/layjava/system/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,111 @@
+package com.layjava.system.service.impl;
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.layjava.system.domain.SysUser;
+import com.layjava.system.domain.bo.SysUserBo;
+import com.layjava.system.domain.vo.SysUserVo;
+import com.layjava.system.mapper.SysUserMapper;
+import com.layjava.system.service.SysUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.noear.solon.annotation.Component;
+import org.apache.ibatis.solon.annotation.Db;
+import com.layjava.common.mybatis.core.page.PageQuery;
+import com.layjava.common.mybatis.core.page.PageResult;
+import com.layjava.common.core.util.MapstructUtils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * 用户信息表 服务实现类
+ *
+ * @author chengliang4810
+ * @since 2024-04-24
+ */
+@Slf4j
+@Component
+public class SysUserServiceImpl  implements SysUserService {
+
+    @Db
+    private SysUserMapper sysUserMapper;
+
+    @Override
+    public List<SysUserVo> getSysUserVoList(SysUserBo sysUserBo) {
+        Wrapper<SysUser> sysUserWrapper = buildWrapper(sysUserBo);
+        return sysUserMapper.selectVoList(sysUserWrapper);
+    }
+
+    @Override
+    public PageResult<SysUserVo> getSysUserVoList(SysUserBo sysUserBo, PageQuery pageQuery) {
+        Wrapper<SysUser> sysUserWrapper = buildWrapper(sysUserBo);
+
+        IPage<SysUserVo> voPage = sysUserMapper.selectVoPage(pageQuery.build(), sysUserWrapper);
+        return PageResult.build(voPage);
+    }
+
+    @Override
+    public SysUserVo getSysUserVoById(Long id) {
+        return sysUserMapper.selectVoById(id);
+    }
+
+    @Override
+    public boolean saveSysUser(SysUserBo sysUserBo) {
+        // 参数校验
+        Assert.notNull(sysUserBo, "用户信息表不能为空");
+
+        SysUser sysUser = MapstructUtils.convert(sysUserBo, SysUser.class);
+        return sysUserMapper.insert(sysUser) > 0;
+    }
+
+    @Override
+    public boolean updateSysUserById(SysUserBo sysUserBo) {
+        // 参数校验
+        Assert.notNull(sysUserBo, "用户信息表不能为空");
+        Assert.notNull(sysUserBo.getUserId(), "用户信息表ID不能为空" );
+
+        SysUser sysUser = MapstructUtils.convert(sysUserBo, SysUser.class);
+        return sysUserMapper.updateById(sysUser) > 0;
+    }
+
+    @Override
+    public int deleteSysUserById(List<Long> idList) {
+        // 参数校验
+        Assert.notEmpty(idList, "用户信息表ID不能为空");
+
+        return sysUserMapper.deleteBatchIds(idList);
+    }
+
+    /**
+     * 构建查询条件
+     */
+    private Wrapper<SysUser> buildWrapper(SysUserBo sysUserBo) {
+        Map<String, Object> params = sysUserBo.getParams();
+        LambdaQueryWrapper<SysUser> queryWrapper = Wrappers.lambdaQuery();
+        // 条件构造
+        queryWrapper.eq(sysUserBo.getUserId() != null, SysUser::getUserId, sysUserBo.getUserId());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getTenantId()), SysUser::getTenantId, sysUserBo.getTenantId());
+        queryWrapper.eq(sysUserBo.getDeptId() != null, SysUser::getDeptId, sysUserBo.getDeptId());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getUserName()), SysUser::getUserName, sysUserBo.getUserName());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getNickName()), SysUser::getNickName, sysUserBo.getNickName());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getCertificateNumber()), SysUser::getCertificateNumber, sysUserBo.getCertificateNumber());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getUserType()), SysUser::getUserType, sysUserBo.getUserType());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getEmail()), SysUser::getEmail, sysUserBo.getEmail());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getPhonenumber()), SysUser::getPhonenumber, sysUserBo.getPhonenumber());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getSex()), SysUser::getSex, sysUserBo.getSex());
+        queryWrapper.eq(sysUserBo.getAvatar() != null, SysUser::getAvatar, sysUserBo.getAvatar());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getPassword()), SysUser::getPassword, sysUserBo.getPassword());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getStatus()), SysUser::getStatus, sysUserBo.getStatus());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getDelFlag()), SysUser::getDelFlag, sysUserBo.getDelFlag());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getLoginIp()), SysUser::getLoginIp, sysUserBo.getLoginIp());
+        queryWrapper.eq(sysUserBo.getLoginDate() != null, SysUser::getLoginDate, sysUserBo.getLoginDate());
+        queryWrapper.eq(StrUtil.isNotBlank(sysUserBo.getRemark()), SysUser::getRemark, sysUserBo.getRemark());
+        return queryWrapper;
+    }
+
+}
