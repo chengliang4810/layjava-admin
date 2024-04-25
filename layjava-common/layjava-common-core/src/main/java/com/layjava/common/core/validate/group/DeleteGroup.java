fee410d3900538e2404a98dbb5c9839f93d36313
chengliang <chengliang4810@163.com>
2024-04-25 09:53:25 +0800

    feat: 添加校验分组

---
+++ layjava-common/layjava-common-core/src/main/java/com/layjava/common/core/validate/group/DeleteGroup.java
@@ -0,0 +1,10 @@
+package com.layjava.common.core.validate.group;
+
+/**
+ * 删除组
+ *
+ * @author chengliang
+ * @since  2024/04/25
+ */
+public interface DeleteGroup {
+}
---
+++ layjava-common/layjava-common-core/src/main/java/com/layjava/common/core/validate/group/QueryGroup.java
@@ -0,0 +1,10 @@
+package com.layjava.common.core.validate.group;
+
+/**
+ * 查询组
+ *
+ * @author chengliang
+ * @since  2024/04/25
+ */
+public interface QueryGroup {
+}
