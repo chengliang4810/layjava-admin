270825f019cb4d727206c6b547c2bca7441f7fa8
chengliang <chengliang4810@163.com>
2024-07-20 18:36:06 +0800

    remove: 删除自动 star 功能

--- layjava-modules/layjava-system/src/main/java/com/layjava/auth/service/impl/SocialAuthStrategy.java
+++ layjava-modules/layjava-system/src/main/java/com/layjava/auth/service/impl/SocialAuthStrategy.java
@@ -3,10 +3,7 @@ package com.layjava.auth.service.impl;
 import cn.dev33.satoken.stp.SaLoginModel;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.http.HttpUtil;
-import cn.hutool.http.Method;
 import com.layjava.auth.domain.vo.LoginVo;
 import com.layjava.auth.service.AuthStrategy;
 import com.layjava.auth.service.AuthStrategyService;
@@ -76,15 +73,6 @@ public class SocialAuthStrategy implements AuthStrategyService {
             throw new ServiceException(response.getMsg());
         }
         AuthUser authUserData = response.getData();
-        if ("GITEE".equals(authUserData.getSource())) {
-            // 如用户使用 gitee 登录顺手 star 给作者一点支持 拒绝白嫖
-            HttpUtil.createRequest(Method.PUT, "https://gitee.com/api/v5/user/starred/dromara/RuoYi-Vue-Plus")
-                    .formStr(MapUtil.of("access_token", authUserData.getToken().getAccessToken()))
-                    .executeAsync();
-            HttpUtil.createRequest(Method.PUT, "https://gitee.com/api/v5/user/starred/dromara/RuoYi-Cloud-Plus")
-                    .formStr(MapUtil.of("access_token", authUserData.getToken().getAccessToken()))
-                    .executeAsync();
-        }
 
         List<SysSocialVo> list = sysSocialService.selectByAuthId(authUserData.getSource() + authUserData.getUuid());
         if (CollUtil.isEmpty(list)) {
