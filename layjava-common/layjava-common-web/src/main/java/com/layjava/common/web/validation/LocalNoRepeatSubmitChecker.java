d027bce002038b1ade832d15e23dd934e634687d
chengliang4810 <chengliang4810@163.com>
2025-01-08 00:36:04 +0800

    feat(validation): 添加本地重复提交检查器
    新增 `LocalNoRepeatSubmitChecker` 类，用于实现本地重复提交检查功能。该检查器通过 `NoRepeatSubmitChecker` 接口实现，确保在指定时间内防止重复提交。

---
+++ layjava-common/layjava-common-web/src/main/java/com/layjava/common/web/validation/LocalNoRepeatSubmitChecker.java
@@ -0,0 +1,19 @@
+package com.layjava.common.web.validation;
+
+import org.noear.solon.annotation.Component;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.validation.annotation.NoRepeatSubmit;
+import org.noear.solon.validation.annotation.NoRepeatSubmitChecker;
+
+/**
+ * 重复提交检查器
+ */
+@Component
+public class LocalNoRepeatSubmitChecker implements NoRepeatSubmitChecker {
+
+    @Override
+    public boolean check(NoRepeatSubmit anno, Context ctx, String submitHash, int limitSeconds) {
+        return true;
+    }
+
+}
