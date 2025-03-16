78602357f53d2773be6b8405ce2cb83163581e96
chengliang4810 <chengliang4810@163.com>
2025-03-16 11:43:42 +0800

    refactor(mybatis): 将PageResult类的泛型参数从T改为V
    统一泛型命名规范，提高代码可读性和一致性。同时更新pom.xml中的solon-parent版本至3.1.0。

--- layjava-common/layjava-common-mybatis/src/main/java/com/layjava/common/mybatis/core/page/PageResult.java
+++ layjava-common/layjava-common-mybatis/src/main/java/com/layjava/common/mybatis/core/page/PageResult.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class PageResult<T> extends R<PageInfo<T>> implements Serializable {
+public class PageResult<V> extends R<PageInfo<V>> implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -31,28 +31,28 @@ public class PageResult<T> extends R<PageInfo<T>> implements Serializable {
      * @param list  列表数据
      * @param total 总记录数
      */
-    public PageResult(List<T> list, long total) {
+    public PageResult(List<V> list, long total) {
         setData(PageInfo.build(list, total));
     }
 
-    public static <T> PageResult<T> build(Page<T> page) {
-        PageResult<T> rspData = new PageResult<>();
+    public static <V> PageResult<V> build(Page<V> page) {
+        PageResult<V> rspData = new PageResult<>();
         rspData.setCode(HttpStatus.HTTP_OK);
         rspData.setMsg("查询成功" );
         rspData.setData(PageInfo.build(page.getRecords(), page.getTotalRow()));
         return rspData;
     }
 
-    public static <T> PageResult<T> build(List<T> list) {
-        PageResult<T> rspData = new PageResult<>();
+    public static <V> PageResult<V> build(List<V> list) {
+        PageResult<V> rspData = new PageResult<>();
         rspData.setCode(HttpStatus.HTTP_OK);
         rspData.setMsg("查询成功" );
         rspData.setData(PageInfo.build(list, list.size()));
         return rspData;
     }
 
-    public static <T> PageResult<T> build() {
-        PageResult<T> rspData = new PageResult<>();
+    public static <V> PageResult<V> build() {
+        PageResult<V> rspData = new PageResult<>();
         rspData.setCode(HttpStatus.HTTP_OK);
         rspData.setMsg("查询成功" );
         return rspData;
