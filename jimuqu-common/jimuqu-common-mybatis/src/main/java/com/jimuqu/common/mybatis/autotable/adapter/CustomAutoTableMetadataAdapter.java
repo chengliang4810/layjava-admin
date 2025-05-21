44961f4c2b268f934811cc90b7958aa5d4adf9e4
chengliang4810 <chengliang4810@163.com>
2025-05-21 22:40:18 +0800

    refactor(mybatis): 移除AutoColumn注解并增强CustomAutoTableMetadataAdapter
    移除BaseEntity和GenTable中的AutoColumn注解，改用Javadoc生成表注释
    增强CustomAutoTableMetadataAdapter功能，支持从Javadoc解析列类型、长度和默认值
    优化主键判断逻辑，增加对字段名称为ID的自动识别

--- jimuqu-common/jimuqu-common-mybatis/src/main/java/com/jimuqu/common/mybatis/autotable/adapter/CustomAutoTableMetadataAdapter.java
+++ jimuqu-common/jimuqu-common-mybatis/src/main/java/com/jimuqu/common/mybatis/autotable/adapter/CustomAutoTableMetadataAdapter.java
@@ -1,18 +1,25 @@
 package com.jimuqu.common.mybatis.autotable.adapter;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.map.MapUtil;
 import cn.xbatis.db.IdAutoType;
 import cn.xbatis.db.annotations.Ignore;
 import cn.xbatis.db.annotations.Ignores;
 import cn.xbatis.db.annotations.TableId;
-import com.github.therapi.runtimejavadoc.ClassJavadoc;
-import com.github.therapi.runtimejavadoc.CommentFormatter;
-import com.github.therapi.runtimejavadoc.FieldJavadoc;
-import com.github.therapi.runtimejavadoc.RuntimeJavadoc;
+import com.github.therapi.runtimejavadoc.*;
+import org.dromara.autotable.annotation.ColumnDefault;
+import org.dromara.autotable.annotation.ColumnType;
+import org.dromara.autotable.annotation.enums.DefaultValueEnum;
 import org.dromara.autotable.core.AutoTableMetadataAdapter;
 import org.dromara.autotable.solon.util.AnnotatedElementUtilsPlus;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * 自定义自动表元数据适配器
@@ -22,7 +29,26 @@ import java.util.Arrays;
  */
 public class CustomAutoTableMetadataAdapter implements AutoTableMetadataAdapter {
 
+    /**
+     * 注释格式化器
+     */
     private static final CommentFormatter formatter = new CommentFormatter();
+    /**
+     * 列类型标签
+     */
+    private static final String COLUMN_TYPE_TAG = "type";
+    /**
+     * 列长度标签
+     */
+    private static final String COLUMN_LENGTH_TAG = "length";
+    /**
+     * 列小数长度标签
+     */
+    private static final String COLUMN_DECIMAL_LENGTH_TAG = "decimalLength";
+    /**
+     * 列默认值标签
+     */
+    private static final String COLUMN_DEFAULT_VALUE_TAG = "default";
 
     /**
      * 忽略字段
@@ -33,7 +59,6 @@ public class CustomAutoTableMetadataAdapter implements AutoTableMetadataAdapter 
      */
     @Override
     public boolean isIgnoreField(Field field, Class<?> clazz) {
-
         Ignore ignore = AnnotatedElementUtilsPlus.findDeepMergedAnnotation(field, Ignore.class);
         if (ignore != null) {
             return true;
@@ -56,15 +81,16 @@ public class CustomAutoTableMetadataAdapter implements AutoTableMetadataAdapter 
      */
     @Override
     public boolean isPrimary(Field field, Class<?> clazz) {
+        // 通过TableId判断是否为主键
         if (AnnotatedElementUtilsPlus.findDeepMergedAnnotation(field, TableId.class) != null) {
             return true;
         }
-
+        // 字段名如果为 ID 则认为是主键
         return "id".equals(field.getName());
     }
 
     /**
-     * 是自动递增
+     * 自动递增
      *
      * @param field 字段
      * @param clazz 类型
@@ -83,6 +109,7 @@ public class CustomAutoTableMetadataAdapter implements AutoTableMetadataAdapter 
 
     /**
      * 表注释
+     *
      * @param clazz 实体类
      * @return
      */
@@ -97,6 +124,7 @@ public class CustomAutoTableMetadataAdapter implements AutoTableMetadataAdapter 
 
     /**
      * 字段注释
+     *
      * @param field 字段
      * @param clazz 实体类
      * @return 字段注释
@@ -104,9 +132,93 @@ public class CustomAutoTableMetadataAdapter implements AutoTableMetadataAdapter 
     @Override
     public String getColumnComment(Field field, Class<?> clazz) {
         FieldJavadoc fieldJavadoc = RuntimeJavadoc.getJavadoc(field);
-        if (fieldJavadoc!= null &&!fieldJavadoc.isEmpty()) {
+        if (fieldJavadoc != null && !fieldJavadoc.isEmpty()) {
             return formatter.format(fieldJavadoc.getComment());
         }
         return null;
     }
+
+    @Override
+    public ColumnDefault getColumnDefaultValue(Field field, Class<?> clazz) {
+        FieldJavadoc fieldJavadoc = RuntimeJavadoc.getJavadoc(field);
+        if (fieldJavadoc != null && !fieldJavadoc.isEmpty()) {
+            Map<String, String> columnTypeMap = parseColumnTypeMap(fieldJavadoc);
+            String value = Optional.ofNullable(columnTypeMap.get(COLUMN_DEFAULT_VALUE_TAG)).orElse("");
+            return new ColumnDefault() {
+                @Override
+                public DefaultValueEnum type() {
+                    return DefaultValueEnum.UNDEFINED;
+                }
+
+                @Override
+                public String value() {
+                    return value;
+                }
+
+                @Override
+                public Class<? extends Annotation> annotationType() {
+                    return ColumnDefault.class;
+                }
+            };
+        }
+        return null;
+    }
+
+    @Override
+    public ColumnType getColumnType(Field field, Class<?> clazz) {
+        FieldJavadoc fieldJavadoc = RuntimeJavadoc.getJavadoc(field);
+        if (fieldJavadoc != null && !fieldJavadoc.isEmpty()) {
+            Map<String, String> columnTypeMap = parseColumnTypeMap(fieldJavadoc);
+            if (CollUtil.isEmpty(columnTypeMap)) {
+                return null;
+            }
+
+            return new ColumnType(){
+                @Override
+                public Class<? extends Annotation> annotationType() {
+                    return ColumnType.class;
+                }
+
+                @Override
+                public String value() {
+                    return Optional.ofNullable(columnTypeMap.get(COLUMN_TYPE_TAG)).orElse("");
+                }
+
+                @Override
+                public int length() {
+                    return Optional.ofNullable(columnTypeMap.get(COLUMN_LENGTH_TAG)).map(Integer::parseInt).orElse(-1);
+                }
+
+                @Override
+                public int decimalLength() {
+                    return Optional.ofNullable(columnTypeMap.get(COLUMN_DECIMAL_LENGTH_TAG)).map(Integer::parseInt).orElse(-1);
+                }
+
+                @Override
+                public String[] values() {
+                    return new String[]{};
+                }
+            };
+        }
+
+        return null;
+    }
+
+    /**
+     * 解析列类型映射
+     *
+     * @param fieldJavadoc 字段文档
+     * @return {@link Map}<{@link String}, {@link String}>
+     * 列类型映射
+     */
+    private Map<String, String> parseColumnTypeMap(FieldJavadoc fieldJavadoc) {
+        List<OtherJavadoc> otherTagList = fieldJavadoc.getOther();
+        if (CollUtil.isEmpty(otherTagList)) {
+            return MapUtil.empty();
+        }
+
+        return otherTagList.stream().filter(otherJavadoc -> otherJavadoc.getComment() != null)
+                .collect(Collectors.toMap(OtherJavadoc::getName, otherJavadoc -> otherJavadoc.getComment().toString()));
+    }
+
 }
--- jimuqu-common/jimuqu-common-mybatis/src/main/java/com/jimuqu/common/mybatis/core/entity/BaseEntity.java
+++ jimuqu-common/jimuqu-common-mybatis/src/main/java/com/jimuqu/common/mybatis/core/entity/BaseEntity.java
@@ -5,7 +5,6 @@ import cn.xbatis.db.IdAutoType;
 import cn.xbatis.db.annotations.TableField;
 import cn.xbatis.db.annotations.TableId;
 import lombok.Data;
-import org.dromara.autotable.annotation.AutoColumn;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -28,41 +27,35 @@ public class BaseEntity implements Serializable {
      * 主键
      */
     @TableId(value = IdAutoType.GENERATOR, generatorName = IdentifierGeneratorType.DEFAULT)
-    @AutoColumn(comment = "主键")
     protected Long id;
 
     /**
      * 创建部门
      */
-    @AutoColumn(comment = "创建部门")
     @TableField(defaultValue = "{CURRENT_DEPT_ID}")
     protected Long createDept;
 
     /**
      * 创建者
      */
-    @AutoColumn(comment = "创建者")
     @TableField(defaultValue = "{CURRENT_USER_ID}")
     protected Long createBy;
 
     /**
      * 创建时间
      */
-    @AutoColumn(comment = "创建时间")
     @TableField(defaultValue = "{NOW}")
     protected Date createTime;
 
     /**
      * 更新者
      */
-    @AutoColumn(comment = "更新者")
     @TableField(defaultValue = "{CURRENT_USER_ID}", updateDefaultValue = "{CURRENT_USER_ID}")
     protected Long updateBy;
 
     /**
      * 更新时间
      */
-    @AutoColumn(comment = "更新时间")
     @TableField(defaultValue = "{NOW}", updateDefaultValue = "{NOW}")
     protected Date updateTime;
 
